827234421ef33b88c9ce1981d1162fbc8225a068
zhouwenting <1191574024@qq.com>
2019-11-06 11:37:19 +0800

    ''

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,13 @@
 import axios from './axios';
 
 // 登录信息
-export default function handleLogin(username, password) {
+export  function Login(username, password) {
     return axios.post('/api/login', {
         username,
         password
     })
 }
 
-export default function getList() {
+export  function getList() {
     return axios.get('/api/queryData')
 }
